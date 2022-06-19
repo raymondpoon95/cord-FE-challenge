8a6a30c9d8b7c28746af5d4e909b7ff225902078
Raymond Poon <raymondpoon@Raymonds-MacBook-Pro.local>
2022-06-19 23:38:46 +0100

    added filter checkbox component and functionality for genres, also made responsive

--- src/components/accordionfilter/index.js
+++ src/components/accordionfilter/index.js
@@ -1,10 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Checkbox from "../checkbox";
 
-const AccordionFilter = () => {
-  //
-  return <Checkbox />;
+const AccordionFilter = ({ genres, ratings, languages, onChange }) => {
+  const [openGenre, setOpenGenre] = useState(false);
+  const [openRating, setOpenRating] = useState(false);
+  const [openLanguage, setOpenLanguage] = useState(false);
+
+  return (
+    <GenreContainer>
+      {/* Select genre */}
+      <SubHeading>
+        <ExpandButtons onClick={() => setOpenGenre((prevState) => !prevState)}>
+          {openGenre ? <>&#8722;</> : <>&#43;</>}
+        </ExpandButtons>
+        Select Genre(s)
+      </SubHeading>
+      <ContentContainer className={openGenre ? "visible" : ""}>
+        {genres &&
+          genres.map((genre) => (
+            <Checkbox
+              key={genre.name}
+              id={genre.id}
+              name={genre.name}
+              label={genre.name}
+              open={openGenre}
+              onChange={onChange}
+              heading="genre"
+            />
+          ))}
+      </ContentContainer>
+
+      {/* Select rating */}
+      <SubHeading>
+        <ExpandButtons onClick={() => setOpenRating((prevState) => !prevState)}>
+          {openRating ? <>&#8722;</> : <>&#43;</>}
+        </ExpandButtons>
+        Select Rating
+      </SubHeading>
+      <ContentContainer className={openRating ? "visible" : ""}>
+        {ratings &&
+          ratings.map((rating) => (
+            <Checkbox
+              key={rating.name}
+              id={rating.id}
+              name={rating.name}
+              label={rating.name}
+              open={openRating}
+              onChange={onChange}
+              heading="rating"
+            />
+          ))}
+      </ContentContainer>
+
+      {/* Select language */}
+      <SubHeading>
+        <ExpandButtons
+          onClick={() => setOpenLanguage((prevState) => !prevState)}
+        >
+          {openLanguage ? <>&#8722;</> : <>&#43;</>}
+        </ExpandButtons>
+        Select Language
+      </SubHeading>
+      <ContentContainer className={openLanguage ? "visible" : ""}>
+        {languages &&
+          languages.map((language) => (
+            <Checkbox
+              key={language.name}
+              id={language.id}
+              name={language.name}
+              label={language.name}
+              open={openLanguage}
+              onChange={onChange}
+              heading="language"
+            />
+          ))}
+      </ContentContainer>
+    </GenreContainer>
+  );
 };
 
 export default AccordionFilter;
+
+const GenreContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubHeading = styled.h2`
+  display: flex;
+  align-items: center;
+`;
+
+const ExpandButtons = styled.span`
+  font-size: 2rem;
+  padding-right: 10px;
+  cursor: pointer;
+`;
+
+const ContentContainer = styled.div`
+  opacity: 0;
+  height: 0px;
+
+  &.visible {
+    opacity: 1;
+    height: auto;
+  }
+`;
--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,18 +1,46 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-
-export default function Checkbox ({ id, name, checked, label, onChange }) {
+export default function Checkbox({
+  id,
+  name,
+  checked,
+  label,
+  onChange,
+  open,
+  heading,
+}) {
   // TODO: Style the component and checkmark to look like the mockup provided
   return (
-    <CheckboxCont>
-      <input type="checkbox" id={id} name={name} checked={checked} onChange={e => onChange(e.target.checked)}></input>
+    <CheckboxCont className={open ? "active" : ""}>
+      <input
+        type="checkbox"
+        id={id}
+        name={name}
+        checked={checked}
+        onChange={(e) => onChange(e.target.id, heading)}
+      ></input>
       <label htmlFor={id}>{label}</label>
     </CheckboxCont>
-  )
-
+  );
 }
 
 const CheckboxCont = styled.div`
   position: relative;
-`
+  padding: 5px 0;
+  font-weight: 300;
+  opacity: 0;
+  transform: translateY(-10px);
+  transition: opacity 450ms ease-in-out, transform 450ms ease-in-out;
+  pointer-events: none;
+
+  &.active {
+    opacity: 1;
+    transform: translateY(0);
+    pointer-events: auto;
+  }
+
+  label {
+    padding-left: 10px;
+  }
+`;
